62a011ecfd8fe93d643b16c19cedc955ae7ad85a
Piero Terreros <piertf14@gmail.com>
2019-06-18 00:28:33 -0500

    Fix register

--- socceruser/views.py
+++ socceruser/views.py
@@ -2,6 +2,9 @@ from socceruser.models import MyUser
 from socceruser.serializers import MyUserSerializer
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 
 class MyUserList(generics.ListCreateAPIView):
@@ -15,3 +18,16 @@ class MyUserDetail(generics.RetrieveUpdateDestroyAPIView):
 	serializer_class = MyUserSerializer
 	permission_classes = (AllowAny,)
 
+
+class MyUserApiView(APIView):
+	permission_classes = [AllowAny]
+
+	def post(self, request, format=None):
+		serializer = MyUserSerializer(data=request.data)
+		if serializer.is_valid():
+			myuser = serializer.save()
+			myuser.set_password(myuser.password)
+			myuser.save()
+			return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
