5b0fb4aa798c70540d433661b466867c9e988b07
Piero Terreros <piertf14@gmail.com>
2019-06-17 22:03:05 -0500

    Fix pep-8

--- socceruser/serializers.py
+++ socceruser/serializers.py
@@ -1,6 +1,6 @@
-#from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 from socceruser.models import MyUser
+
 
 class MyUserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
