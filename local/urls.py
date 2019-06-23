ebc7107a79d75f63ac76d688278260893f0c4533
Piero Terreros <piertf14@gmail.com>
2019-06-23 02:12:17 -0500

    Add local list

--- local/urls.py
+++ local/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('local/', views.LocalAPI.as_view()),
+    path('local/<int:pk>/', views.LocalAPI.as_view()),
     path('court-soccer/', views.CourtSoccerAPI.as_view()),
 ]
--- local/views.py
+++ local/views.py
@@ -3,6 +3,7 @@ from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 
+from local.models import Local
 from local.serializers import LocalSerializer, CourtSoccerSerializer
 from socceruser.utils import get_access_token
 
@@ -19,6 +20,19 @@ class LocalAPI(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request, pk=None, format=None):
+        user = get_access_token(request).user
+        locales = user.user_local.all()
+        if pk:
+            try:
+                local = Local.objects.get(pk=pk, user=user)
+                serializer = LocalSerializer(local, many=False)
+                return Response(serializer.data)
+            except Local.DoesNotExist as exe:
+                return Response({str(exe)}, status=400)
+        serializer = LocalSerializer(locales, many=True)
+        return Response(serializer.data)
 
 
 class CourtSoccerAPI(APIView):
